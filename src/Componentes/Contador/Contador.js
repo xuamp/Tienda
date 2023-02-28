f449f00e5cd45601e996e74f056091a2f6fbb98b
xuamp <108774140+xuamp@users.noreply.github.com>
2023-02-27 23:10:29 -0300

    Add files via upload

--- src/Componentes/Contador/Contador.js
+++ src/Componentes/Contador/Contador.js
@@ -1,16 +1,36 @@
 import "../Contador/Contador.css";
 import { CgMathMinus, CgMathPlus } from "react-icons/cg";
+import { useState } from "react";
 
 function Contador() {
+  const [contador, setContador] = useState(1);
+  const stock = 10;
+
+  function sumar() {
+    if (contador === stock) {
+      return undefined;
+    } else {
+      setContador(contador + 1);
+    }
+  }
+
+  function restar() {
+    if (contador === 0) {
+      return undefined;
+    } else {
+      setContador(contador - 1);
+    }
+  }
+
   return (
     <div className="Contador">
       <div className="sumador">
-        <CgMathPlus />
-        <p>5</p>
-        <CgMathMinus />
+        <CgMathMinus onClick={restar} className="ico-contador" />
+        <p className="numero">{contador}</p>
+        <CgMathPlus onClick={sumar} className="ico-contador" />
       </div>
       <div>
-        <button>Comprar</button>
+        <button className="boton-comprar">Comprar</button>
       </div>
     </div>
   );
